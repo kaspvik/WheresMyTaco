ad835278cced61ad7cd5169ec8a817efb1c5fa33
kaspvik <kasper.vikstrom@medieinstituet.se>
2024-11-11 15:07:39 +0100

    first js function, testing the function

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 window.addEventListener("DOMContentLoaded", main); // motsvarar defer i html
+
+
+
+function showFirstSceneSmulan() {
+    const homePage = document.getElementById("homePage");
+    const firstSceneSmulan = document.getElementById("firstSceneSmulan");
+
+    homePage.style.display = "none";
+    firstScene.style.display = "block";
+}
+
+function loadStartScene() {
+
+}
