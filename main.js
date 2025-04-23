ee814c6cff2a90ea52dbcf78d8c39844720568cd
Kasper Vikström <kasper.vik@hotmail.com>
2025-04-23 12:49:40 +0200

    Refactor button styles and improve scene display logic

--- main.js
+++ main.js
@@ -6,26 +6,33 @@ function main() {
 // Array för alla rum
 const scenes = ["homePage", "firstSceneSmulan", "secondSceneSmulan", "thirdSceneSmulan", "inTheKitchen", "findTheBowl", "bowlAlternative", "underTheTable", "onTable", "onTableSecond", "crumbsOnTable", "onTableThird", "overviewOverAllRooms", "bedroomBeforeItem", "bedroomAfterItem", "livingroomBeforeItem", "livingroomAfterItem", "bathroomBeforeItem", "bathroomAfterItem", "atticBeforeItem", "atticAfterItem", "closetBeforeItem", "closetAfterItem", "theNoteInCloset", "lastScene", "thanksForPlaying"];
 
+window.onload = function () {
+    showScene("homePage");
+};
+
 // Byta scenfunktion
 function showScene(sceneId) {
-
     scenes.forEach(id => {
-        document.getElementById(id).style.display = (id === sceneId) ? "block" : "none";
+        const scene = document.getElementById(id);
+        if (scene) {
+            scene.style.display = (id === sceneId) ? "block" : "none";
+        }
     });
 }
+
+
+const requiredItems = ['nachoBag', 'salsaJar', 'theVeggies', 'pileOfTortillas'];
 
 // Array för att lagra insamlade föremål
 let collectedItems = [];
 
 // Funktion för att plocka upp föremålet
 function pickUpItem(itemId) {
-    // Kontrollera om föremålet redan är samlat
     if (!collectedItems.includes(itemId)) {
-        // Lägg till föremålet i arrayen
         collectedItems.push(itemId);
         console.log(`${itemId} Its now in ypur inventory:`, collectedItems);
 
-        // Dölj föremålet från skärmen
+
         const item = document.getElementById(itemId);
         if (item) {
             item.style.display = 'none';
